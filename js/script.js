a6b6a3d5f55b084df66e04ab5bae327394eef03b
Gustavo Araujo Borges <gustavoaraujob1+1@gmail.com>
2021-05-16 14:43:25 -0300

    Changed setup parameters

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const canvas = document.getElementById("renderCanvas");
 
-const engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true, disableWebGL2Support: false });
+const engine = new BABYLON.Engine(canvas);
 
 const scene = new BABYLON.Scene(engine);
 
-const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+const camera = new BABYLON.ArcRotateCamera("camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0));
+camera.setPosition(new BABYLON.Vector3(3, 3, 3));
 camera.attachControl(canvas, true);
 
 const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
 
+// 1 - HighMap
 const box = BABYLON.MeshBuilder.CreateBox("box", {});
 
 engine.runRenderLoop(() => {
