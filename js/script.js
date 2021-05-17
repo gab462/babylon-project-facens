1818a991c3d2e275c6f98715d1aa874ff0641244
Gustavo Araujo Borges <gustavoaraujob1+1@gmail.com>
2021-05-17 19:14:43 -0300

    Ex1 - HighMap

--- js/script.js
+++ js/script.js
@@ -13,8 +13,16 @@ camera.attachControl(canvas, true);
 
 const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
 
-// 1 - HighMap
-const box = BABYLON.MeshBuilder.CreateBox("box", {});
+// 1, 6 - HighMap
+var ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap("ground", "res/ground.png", { radius: 0, subdivisions: 50, minHeight: 0, maxHeight: 10 });
+ground.material = new BABYLON.StandardMaterial("groundMaterial", scene);
+ground.material.diffuseTexture = new BABYLON.Texture("res/ground-image.png", scene);
+// ground.material.opacityTexture = new BABYLON.Texture("res/ground-image.png", scene);
+// ground.material.opacityTexture.hasAlpha = true;
+
+ground.scaling = new BABYLON.Vector3(10,1,10);
+
+ground.checkCollisions = true;
 
 engine.runRenderLoop(() => {
     scene.render();
