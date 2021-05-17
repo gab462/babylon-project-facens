b20932056d23e268477e9c5c839559ff2f3fc25f
Gustavo Araujo Borges <gustavoaraujob1+1@gmail.com>
2021-05-17 19:14:11 -0300

    Changed parameters for scene / engine / camera

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const canvas = document.getElementById("renderCanvas");
 
 const engine = new BABYLON.Engine(canvas);
+engine.enableOfflineSupport = false;
 
 const scene = new BABYLON.Scene(engine);
+scene.collisionsEnabled = true;
+scene.enablePhysics();
 
 const camera = new BABYLON.ArcRotateCamera("camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0));
-camera.setPosition(new BABYLON.Vector3(3, 3, 3));
+camera.setPosition(new BABYLON.Vector3(-5, 15, -5));
 camera.attachControl(canvas, true);
 
 const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
